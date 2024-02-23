b4901aef046ab5ca79c0d9a6e1755c1ef563d3d1
fandok <fandika.okdiba@gmail.com>
2024-02-23 21:19:45 +0700

    adjust sign in image

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -3,21 +3,19 @@ import SignInCars from "../assets/SignInCars.png";
 import SignInForm from "../Components/SignInForm/SignInForm";
 
 function SignIn() {
-    return (
-        <>
-            <Row className="g-0">
-                <Col xs="6">
-                        <img src={SignInCars} alt="Cars" />
-                </Col>
+  return (
+    <Row className="g-0">
+      <Col xs="6">
+        <img style={{ height: "100vh" }} src={SignInCars} alt="Cars" />
+      </Col>
 
-                <Col xs="1" />
+      <Col xs="1" />
 
-                <Col xs="5">
-                    <SignInForm />
-                </Col>
-            </Row>
-        </>
-    );
-};
+      <Col xs="5">
+        <SignInForm />
+      </Col>
+    </Row>
+  );
+}
 
 export default SignIn;
